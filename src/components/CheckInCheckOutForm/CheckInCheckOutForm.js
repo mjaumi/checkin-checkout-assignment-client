b276b2a4eccf1b222583e3f0c9ca310f2e55b446
mjaumi <mjaumi2864@gmail.com>
2022-06-01 19:33:48 +0600

    initial commit

---
+++ src/components/CheckInCheckOutForm/CheckInCheckOutForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CheckInCheckOutForm = () => {
+
+    // rendering calendar component here 
+    return (
+        <section>
+            <h1>This is forms</h1>
+        </section>
+    );
+};
+
+export default CheckInCheckOutForm;
